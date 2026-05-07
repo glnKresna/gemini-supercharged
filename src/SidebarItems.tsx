3cc7a4845ccb2ec6bcee20c225a3685819e41aa6
glnKresna <prabandikakresna@gmail.com>
2026-05-07 09:33:02 +0700

    chore: restructure codebase files

--- src/SidebarItems.tsx
+++
@@ -1,8 +0,0 @@
-import React, { useState } from 'react';
-
-interface SidebarItemsProps {
-    icon: string;
-    label: string;
-    isActive?: boolean;
-    onClick?: () => void;
-}
--- src/SidebarMenu.tsx
+++
@@ -1,3 +0,0 @@
-import React from "react";
-import {useStore} from "./store/store";
-
